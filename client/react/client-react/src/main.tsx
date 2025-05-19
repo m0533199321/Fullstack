2023a36229b3aced8693e8a2369baede199bcd72
m0533199321 <m0533199321@gmail.com>
2025-05-19 14:55:46 +0300

    try with _redirects

--- client/react/client-react/src/main.tsx
+++ client/react/client-react/src/main.tsx
@@ -1,16 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
+
 if (CLIENT_ID) {
   createRoot(document.getElementById('root')!).render(
     <GoogleOAuthProvider clientId={CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-    // <StrictMode>
-    //   <App />
-    // </StrictMode>,
-  )
+  );
 }
+
+
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+// import { GoogleOAuthProvider } from '@react-oauth/google'
+
+// const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
+// if (CLIENT_ID) {
+//   createRoot(document.getElementById('root')!).render(
+//     <GoogleOAuthProvider clientId={CLIENT_ID}>
+//       <App />
+//     </GoogleOAuthProvider>
+//     // <StrictMode>
+//     //   <App />
+//     // </StrictMode>,
+//   )
+// }
